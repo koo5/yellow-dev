c26273d472421d63b89eb538df9cbb443c4369c7
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-10-27 01:45:28 +0200

    docker..

--- commit.sh
+++ commit.sh
@@ -1,37 +1,41 @@
 #!/bin/fish
 
+function e; or status --is-interactive; or exit 1; end
+
 set m $argv[1]
 
 echo $m
 
+git status; e
+
 cd yellow-server-common/
 pwd; git status;
-gcam $m; gp
+gcam $m; gp; e
 cd ..
 
 cd yellow-server/src
 pwd; git status;
-../../link.sh
-gcam $m; gp
+../../link.sh; e
+gcam $m; gp; e
 cd ../..
 
 cd yellow-server-module-messages/src
 pwd; git status;
-../../link.sh
-gcam $m; gp
+../../link.sh; e
+gcam $m; gp; e
 cd ../..
 
 cd yellow-admin/
 pwd; git status;
-../link.sh
-gcam $m; gp
+../link.sh; e
+gcam $m; gp; e
 cd ..
  
 cd yellow-client/
 pwd; git status;
-gcam $m; gp
+gcam $m; gp; e
 cd ..
 
 pwd; git status;
-gcam $m; gp
+gcam $m; gp; e
 git status
