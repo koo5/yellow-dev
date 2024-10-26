a4a18b868e25eecd1304b141de93f7c88b6426de
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-10-27 01:42:29 +0200

    docker..

--- link.sh
+++ link.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env fish
 
 nvm use; bun remove yellow-server-common;  bun add https://github.com/libersoft-org/yellow-server-common --latest; rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common
