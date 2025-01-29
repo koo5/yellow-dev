3baf919e3785b73101d5d5a62e0913438e8154ec
Your Name <koom@c1>
2025-01-29 18:17:10 +0100

    fix module logging config

--- dev_common_link.sh
+++ dev_common_link.sh
@@ -2,5 +2,5 @@
 
 function e; or status --is-interactive; or exit 1; end
 
-nvm use; e; bun remove yellow-server-common; e; bun add https://github.com/libersoft-org/yellow-server-common --latest; e; rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common; e
+nvm use; bun remove yellow-server-common; e; bun add https://github.com/libersoft-org/yellow-server-common --latest; e; rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common; e
 
