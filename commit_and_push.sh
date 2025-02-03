da8cb9fe5c6baa459fa6a97868252d073ada401f
Your Name <koom@c1>
2025-02-03 15:59:56 +0100

    ..

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -8,23 +8,23 @@ nvm use;
 
 
 # Check for commit message
-if test (count $argv) -eq 0
-    # No message provided, open mcedit to write one interactively
-    set temp_msg_file /tmp/commit_message.txt
-    set temp_msg_file2 /tmp/commit_message2.txt
-    echo "# Write your commit message above. Lines starting with '#' will be ignored." > $temp_msg_file
-    mcedit $temp_msg_file
-    # Read the message back and strip out comments
-    grep -v '^#' $temp_msg_file | string trim > $temp_msg_file2
-    if test -z (cat $temp_msg_file2)
-        echo "No commit message provided. Aborting."
-        exit 1
-    end
-    set m "-F" $temp_msg_file2
-else
-    # Use the provided message
-    set m "-m" $argv[1]
-end
+  if test (count $argv) -eq 0
+      # No message provided, open mcedit to write one interactively
+      set temp_msg_file /tmp/commit_message.txt
+      set temp_msg_file2 /tmp/commit_message2.txt
+      echo "# Write your commit message above. Lines starting with '#' will be ignored." > $temp_msg_file
+      mcedit $temp_msg_file
+      # Read the message back and strip out comments
+      grep -v '^#' $temp_msg_file | string trim > $temp_msg_file2
+      if test -z (cat $temp_msg_file2)
+          echo "No commit message provided. Aborting."
+          exit 1
+      end
+      set m "-F" $temp_msg_file2
+  else
+      # Use the provided message
+      set m "-m" $argv[1]
+  end
 
 echo "commit message arg:" $m
 
@@ -35,16 +35,16 @@ echo ========
 echo
 
 cd yellow-server-common/
-bun bun.lockb > bun.lockb.txt
 pwd; git status;
 gca $m; gp; e
 cd ..
 echo
+
 echo ========
 echo
 
 cd yellow-server/src
-bun bun.lockb > bun.lockb.txt
+#bun bun.lockb > bun.lockb.txt
 pwd; git status;
 ../../dev_common_link.sh; e
 gca $m; gp; e
@@ -54,7 +54,7 @@ echo ========
 echo
 
 cd yellow-server-module-messages/src
-bun bun.lockb > bun.lockb.txt
+#bun bun.lockb > bun.lockb.txt
 pwd; git status;
 ../../dev_common_link.sh; e
 gca $m; gp; e
@@ -64,7 +64,7 @@ echo ========
 echo
 
 cd yellow-admin/
-bun bun.lockb > bun.lockb.txt
+#bun bun.lockb > bun.lockb.txt
 pwd; git status;
 #../dev_common_link.sh; e
 gca $m; gp; e
@@ -74,7 +74,7 @@ echo ========
 echo
  
 cd yellow-client/
-bun bun.lockb > bun.lockb.txt
+#bun bun.lockb > bun.lockb.txt
 pwd; git status;
 gca $m; gp; e
 cd ..
--- dev_common_link.sh
+++ dev_common_link.sh
@@ -2,5 +2,8 @@
 
 function e; or status --is-interactive; or exit 1; end
 
-nvm use; bun remove yellow-server-common; e; bun add https://github.com/libersoft-org/yellow-server-common --latest; e; rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common; e
-
+nvm use;
+bun remove yellow-server-common; e;
+bun add https://github.com/libersoft-org/yellow-server-common --latest; e;
+rm -rf ./node_modules/yellow-server-common;
+ln -s ../../../yellow-server-common ./node_modules/yellow-server-common; e
