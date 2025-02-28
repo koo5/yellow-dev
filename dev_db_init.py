4bfeb0c36c8ab0d4676d81c0c8629520e36f979f
Your Name <koom@c1>
2025-02-28 19:06:46 +0100

    messages2...

--- dev_db_init.py
+++ dev_db_init.py
@@ -12,6 +12,11 @@ GRANT ALL ON yellow.* TO username;
 CREATE USER  IF NOT EXISTS 'yellow_module_org_libersoft_messages' IDENTIFIED BY 'password';
 CREATE DATABASE IF NOT EXISTS yellow_module_org_libersoft_messages;
 GRANT ALL ON yellow_module_org_libersoft_messages.* TO 'yellow_module_org_libersoft_messages';
+
+CREATE USER  IF NOT EXISTS 'messages2' IDENTIFIED BY 'password';
+CREATE DATABASE IF NOT EXISTS messages2;
+GRANT ALL ON messages2.* TO 'messages2';
+
 """)
 
 
--- dev_db_populate.py
+++ dev_db_populate.py
@@ -9,6 +9,7 @@ print(f"""
 USE yellow;
 INSERT INTO admins (username, password) VALUES ('admin', '$argon2id$v=19$m=65536,t=20,p=1$Vmb9bCJSHUOJDiS+amdMkzxTljfkanX0JKsYecdBCkQ$slQjytnGeh4/ScqmXOJ6mjjfdmu/9eVSd6dV032nrm8');
 INSERT INTO modules (name, connection_string) VALUES ('org.libersoft.messages', 'ws://localhost:25001/');
+INSERT INTO modules (name, connection_string) VALUES ('org.libersoft.messages2', 'ws://localhost:25002/');
 INSERT INTO domains (name) VALUES ('{host}');
 
 INSERT INTO users (username, id_domains, visible_name, password) VALUES ('user1', (SELECT id FROM domains WHERE name = '{host}'), 'user1@{host}', '$argon2id$v=19$m=65536,t=20,p=1$Vmb9bCJSHUOJDiS+amdMkzxTljfkanX0JKsYecdBCkQ$slQjytnGeh4/ScqmXOJ6mjjfdmu/9eVSd6dV032nrm8');
