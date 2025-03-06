d3b2b9b395b9eabf380d43d78e108da82f984abb
Your Name <koom@c1>
2025-03-06 20:02:30 +0100

    ..

--- yellow-server-module-dating/src/start-docker-dev.sh
+++ yellow-server-module-dating/src/start-docker-dev.sh
@@ -5,5 +5,6 @@ mkdir -p uploads/message-attachments/
 #[ ! -d "./node_modules/" ] &&
 bun i --frozen-lockfile
 rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common
-bun app.ts --create-database
-bun --watch app.ts
+ls -l
+bun ./main.ts --create-database
+bun --watch ./main.ts
