962bd0fc8e8e37dc771e5f122c0c1fcbdefd44c2
Your Name <koom@c1>
2025-03-13 22:31:42 +0100

    ..

--- scraps/tauri/transparency1/tauri-app/src-tauri/src/lib.rs
+++ scraps/tauri/transparency1/tauri-app/src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,6 +10,19 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            // #[cfg(target_os = "windows")]
+            // apply_blur(&window, Some((18, 18, 18, 125)))
+            //     .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+                
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
