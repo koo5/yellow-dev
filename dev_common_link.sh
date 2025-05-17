cdf0337fe73509e28f7a94cd8d9a52934ed41965
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-05-17 16:05:30 +0200

    Remove Dockerfile auto-creation, update common link script, and mark submodules as dirty
    • In .github/workflows/shared-e2e.yml, removed the automatic placeholder creation when a Dockerfile is missing
    • In dev_common_link.sh, removed commented nvm/bun commands and streamlined the symlink step
    • Appended “-dirty” to submodule commits for yellow-admin, yellow-client, yellow-client-native, yellow-server, and yellow-server-module-messages

--- dev_common_link.sh
+++ dev_common_link.sh
@@ -2,8 +2,5 @@
 
 function e; or status --is-interactive; or exit 1; end
 
-#nvm use;
-#bun remove yellow-server-common; e;
-#bun add https://github.com/libersoft-org/yellow-server-common --latest; e;
 rm -rf ./node_modules/yellow-server-common;
-ln -s ../../../yellow-server-common ./node_modules/yellow-server-common; e
+ln -s ../../../yellow-server-common ./node_modules/yellow-server-common; e;
